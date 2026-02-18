dff8e2ea0925846fd36cdfdf8ffebc1d69ba3557
K1ng-Yike567 <hugaroplay@gmail.com>
2026-02-18 17:09:33 +0100

    Métodos DashBoard
    Métodos para cargar todos los datos en el Dashboard implementados

--- src/main/java/Servicio/ClubService.java
+++ src/main/java/Servicio/ClubService.java
@@ -19,4 +19,64 @@ public class ClubService {
         emf = Persistence.createEntityManagerFactory("club_damaPU");
     }
 
+    // Métodos para cargar todos los datos en el DashBoard
+
+    /**
+     * Método que carga los datos de todos los ComboBox
+     * @Author Hugo
+     */
+    public void cargarDatosDashboard() {
+        cargarSociosDashBoard();
+        cargarPistasDashBoard();
+        cargarReservasDashBoard();
+    }
+
+    /**
+     * Método que carga los socios en el DashBoard
+     * @return Lista de socios
+     * @Author Hugo
+     */
+    public static List<Socio> cargarSociosDashBoard() {
+
+        EntityManager em = emf.createEntityManager();
+
+        Query q = em.createQuery("select s from Socio s");
+        List<Socio> socios = q.getResultList();
+
+        em.close();
+        return socios;
+    }
+
+    /**
+     * Método que carga las pistas en el DashBoard
+     * @return Lista de pistas
+     * @Author Hugo
+     */
+    public static List<Pista> cargarPistasDashBoard() {
+
+        EntityManager em = emf.createEntityManager();
+
+        Query q = em.createQuery("select p from Pista p");
+        List<Pista> pistas = q.getResultList();
+
+        em.close();
+        return pistas;
+    }
+
+    /**
+     * Método que carga las reservas en el DashBoard
+     * @return Lista de reservas
+     * @Author Hugo
+     */
+    public static List<Reserva> cargarReservasDashBoard() {
+
+        EntityManager em = emf.createEntityManager();
+
+        Query q = em.createQuery("select r from Reserva r");
+        List<Reserva> reservas = q.getResultList();
+
+        em.close();
+        return reservas;
+    }
+
 }
